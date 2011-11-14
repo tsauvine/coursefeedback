75e1aa61e1e0843043c624c7a0221266f5d716df
Tapio Auvinen <tapio.auvinen@aalto.fi>
2011-11-14 22:31:48 +0200

    Fixed a route bug

--- config/routes.rb
+++ config/routes.rb
@@ -51,15 +51,15 @@ Rails.application.routes.draw do
   match '/login' => 'sessions#new', :as => :login
   match '/logout' => 'sessions#destroy', :as => :logout
 
-  match ':course/:instance/feedback', :to => 'topics#index', :constraints => { :course => URL_FORMAT_ROUTE, :instance => URL_FORMAT_ROUTE }, :as => 'topic_index'
+  match '/:course/:instance/feedback', :to => 'topics#index', :constraints => { :course => URL_FORMAT_ROUTE, :instance => URL_FORMAT_ROUTE }, :as => 'topic_index'
 
-  match ':course/:instance/new_topic', :to => 'topics#new', :constraints => { :course => URL_FORMAT_ROUTE, :instance => URL_FORMAT_ROUTE }, :as => 'new_topic'
+  match '/:course/:instance/new_topic', :to => 'topics#new', :constraints => { :course => URL_FORMAT_ROUTE, :instance => URL_FORMAT_ROUTE }, :as => 'new_topic'
 
-  match ':course_code/:instance_path/faq', :to => 'faq#index', :constraints => { :course_code => URL_FORMAT_ROUTE, :instance_path => URL_FORMAT_ROUTE }, :as => 'faq'
-  match ':course_code/:instance_path/new_faq', :to => 'faq#new', :constraints => { :course_code => URL_FORMAT_ROUTE, :instance_path => URL_FORMAT_ROUTE }, :as => 'new_faq'
+  match '/:course_code/:instance_path/faq', :to => 'faq#index', :constraints => { :course_code => URL_FORMAT_ROUTE, :instance_path => URL_FORMAT_ROUTE }, :as => 'faq'
+  match '/:course_code/:instance_path/new_faq', :to => 'faq#new', :constraints => { :course_code => URL_FORMAT_ROUTE, :instance_path => URL_FORMAT_ROUTE }, :as => 'new_faq'
 
-  match ':course/:instance' => 'topics#index', :as => 'instance_root', :constraints => { :course => URL_FORMAT_ROUTE, :instance => URL_FORMAT_ROUTE }
-  match ':course_code', :to => 'courses#show', :constraints => { :course_code => URL_FORMAT_ROUTE }, :as => 'course_root'
+  match '/:course/:instance' => 'topics#index', :as => 'instance_root', :constraints => { :course => URL_FORMAT_ROUTE, :instance => URL_FORMAT_ROUTE }
+  match '/:course_code', :to => 'courses#show', :constraints => { :course_code => URL_FORMAT_ROUTE }, :as => 'course_root'
 
   root :to => 'course_instances#index'
 
