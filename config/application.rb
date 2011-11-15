e983bad0e330adb38d221ed9d94fad216c363b25
Tapio Auvinen <tapio.auvinen@aalto.fi>
2011-11-15 11:06:26 +0200

    Changed default language to english

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module Coursefeedback
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.yml')]
-    config.i18n.default_locale = :fi
+    config.i18n.default_locale = :en
 
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
