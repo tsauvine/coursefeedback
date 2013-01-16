9f39a8b0d266b8d980ca63b5831a57d83dc1de64
Tapio Auvinen <tapio.auvinen@aalto.fi>
2013-01-16 14:50:06 +0200

    Fixing error messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  
+  class ActionView::Helpers::FormBuilder
+    # Usage:
+    # form_for(@user) do |f|
+    #   <%= f.hint :name %>
+    def hint(method, options = {})
+      if @object && @object.errors[method]
+        html = "<span class='error'>#{@object.errors[method].join('. ')}</span>"
+        return html.html_safe
+      else
+        html = '<span class="hint">' + @template.t("#{@object_name}_#{method}_hint") + '</span>'
+        return html.html_safe
+      end
+    end
+  end
+  
 end
